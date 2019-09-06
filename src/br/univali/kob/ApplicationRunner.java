92fe3b1f41005c480cf3033581b812af6746a475
gabrielburich <gabrielburich@gmail.com>
2019-09-06 17:31:36 -0300

    Main

---
+++ src/br/univali/kob/ApplicationRunner.java
@@ -0,0 +1,10 @@
+package br.univali.kob;
+
+import br.univali.kob.commandLine.CommandLineCalculator;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        new CommandLineCalculator().runCalculator();
+    }
+}
